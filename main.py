b2f7f5796958c874dabc55fa0c19819a621023f7
Abraham-s-yakubu <110307642+Abraham-s-yakubu@users.noreply.github.com>
2024-07-03 10:19:24 +0100

    Add files via upload
    changes to ip

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ def welcome():
 
 @app.route('/api/hello', methods=['GET'])
 def display():
+    city = "abuja"
     name = get_user_name()
-    ip, city, lat, long = get_user_ip_and_city()
-    whether = get_wheather_info(lat, long)
-    return jsonify({"client_ip": ip, "location": city, "greeting": f"Hello, {name}! the temperature is {whether} degrees Celcius in {city}"})
+    ip = get_user_ip()
+    whether = get_wheather_info(city)
+    return jsonify({"client_ip": ip,
+                    "location": city,
+                    "greeting": f"Hello, {name}! the temperature is {whether} degrees Celcius in {city}"})
 
 
 def get_user_name():
@@ -23,27 +26,15 @@ def get_user_name():
     return username
 
 
-def get_user_ip_and_city():
-    url = "http://ip-api.com/json/"
-    try:
-        responds = requests.get(url)
-        data = responds.json()
-        ip = data["query"]
-        city = data["city"]
-        lat = data["lat"]
-        long = data["lon"]
-        return ip, city, lat, long
-    except requests.exceptions.RequestException as e:
-        print(f"error making API request : {e}")
-        return None
-    except ValueError as e:
-        print(f"Error parsing JSON responds:{e}")
-        return None
+def get_user_ip():
+    ip = request.remote_addr
+    return ip
 
 
-def get_wheather_info(lat, long):
+def get_wheather_info(city):
     api_key = "7300c952042796002a259cc256140402"
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={
+        city}&appid={api_key}&units=metric"
     try:
         responds = requests.get(url)
         responds.raise_for_status()
